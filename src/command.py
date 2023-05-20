86669d93cbe1bda9ec3f40d7f6d5ded7c9f05422
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-05-20 16:13:49 -0700

    p

---
+++ src/command.py
@@ -0,0 +1,9 @@
+import argparse
+
+# POINT OF THIS CLI
+
+# Instead of hard coding how long ago we want the
+# time stamp to be for the rapid API response,
+# we can create a command for this!
+
+# Example -> python main.py --days-included=2
