1adfacb93f2aaa7eb19a7c54fea29ad67a71b8a7
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-09-20 23:38:49 -0700

    Update blacklist->blocklist.

--- blocklist.py
+++ blocklist.py
@@ -1,11 +1,11 @@
-class BlackList:
-    """A class holding methods to determine if a company is blacklisted"""
+class Blockist:
+    """A class holding methods to determine if a company is blocklisted"""
 
-    BLACKLISTED_COMPANIES = set(
+    BLOCKLISTED_COMPANIES = set(
         ["Pattern Learning AI - Career & Tech Recruitment Reimagined!"]
     )
 
     def is_blacklisted_company(self, company: str) -> bool:
         """Determines if the company is blacklisted or not"""
 
-        return True if company in self.BLACKLISTED_COMPANIES else False
+        return True if company in self.BLOCKLISTED_COMPANIES else False
